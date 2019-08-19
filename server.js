0e9c6c4141e1501cd274aa9888013938fc1a7dc5
DobariaArjun <arjun.dobaria12@gmail.com>
2019-08-19 11:50:11 +0530

    change

--- server.js
+++ server.js
@@ -97,7 +97,10 @@ client.connect((err, db) => {
                     }
                     shuffle(result);
                     appArray = result.slice(0,4);
-                    res.send(appArray);
+//                    res.send(appArray);
+                    res.status(200).json({
+		message: "Data received successfully"
+	});
                 }
             });
         })
